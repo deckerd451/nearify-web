9ab1b9a6a8d6f8c11c66d386cd7e017a1b61d24a
Doug Hamilton <Dmhamilton1@live.com>
2026-03-24 09:04:18 -0400

    Update join.js

--- assets/js/join.js
+++ assets/js/join.js
@@ -1,4 +1,4 @@
-import { supabase } from "../supabaseClient.js";
+import { supabase } from "./supabaseClient.js";
 
 function getEventId() {
   const params = new URLSearchParams(window.location.search);
