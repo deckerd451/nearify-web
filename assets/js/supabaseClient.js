a6b6bda85b9ca155d7bb2ef4e0c0f06dabcd7b0f
Doug Hamilton <Dmhamilton1@live.com>
2026-03-24 09:00:47 -0400

    Create supabaseClient.js

---
+++ assets/js/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://unndeygygkgodmmdnlup.supabase.co",
+  "sb_publishable_G0KAfCFTovYCWDeEEKWBfg_8UpPHWWZ"
+);
